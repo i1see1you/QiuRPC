1ff17d087262fddd1e95f77e05a5a61e1dfaabfe
i1see1you <i1see1you@gmail.com>
2016-07-26 18:36:04 +0800

    add magic

--- com.qiusuoba.nettyrpc/src/main/java/com/qiusuoba/nettyrpc/protocol/RpcResponseDecode.java
+++ com.qiusuoba.nettyrpc/src/main/java/com/qiusuoba/nettyrpc/protocol/RpcResponseDecode.java
@@ -6,6 +6,8 @@ import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelHandlerContext;
 import org.jboss.netty.handler.codec.frame.FrameDecoder;
 
+import com.qiusuoba.nettyrpc.common.Constants;
+
 /**
  *响应解码 
  *@Author:caimin 
@@ -17,15 +19,16 @@ public class RpcResponseDecode  extends FrameDecoder {
 	@Override
 	protected Object decode(ChannelHandlerContext context, Channel channel,
 			ChannelBuffer buffer) throws Exception {
-		if (buffer.readableBytes() < 4) {
+		if (buffer.readableBytes() < 2) {
 			return null;
 		}
-		int length = buffer.getInt(buffer.readerIndex());
-		if (buffer.readableBytes() < length + 4) {
-			return null;
+		byte byte1 = buffer.readByte();
+		byte byte2 = buffer.readByte();
+		if (byte1!=Constants.MAGIC_HIGH || byte2!=Constants.MAGIC_LOW) {
+			throw new RuntimeException("magic number not right");
 		}
 		ChannelBufferInputStream in = new ChannelBufferInputStream(buffer);
-		RpcResponse response = ProtobufSerializer.getInstance().decodeResponse(in);
+		RpcResponse response = MySerializerFactory.getInstance(Constants.DEFAULT_RPC_CODE_MODE).decodeResponse(in);
 		return response;
 	}
 }
